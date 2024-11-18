8aefe9b8176ee9e18689ff3977b911039a13b3d9
Kyaw Min Htway <batman066813@gmail.com>
2024-11-18 23:32:00 +0630

    Remove node_modules from tracking

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require("./config/connectDB")
+const router = require('./router/index')
+
+
+const app = express()
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+
+const PORT = process.env.PORT || 8080
+
+app.get("/", (req, res) => {
+    res.json({
+        message : "Server running at " + PORT
+    })
+})
+
+//api endpoints
+app.use("/api",router)
+
+connectDB().then(()=>{
+    app.listen(PORT,() => {
+        console.log("server running at " + PORT)
+    })
+})
